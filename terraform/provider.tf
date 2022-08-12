dd96a0996950e20c02f86377fec7d03a1bfc64d0
ashrujitpal <ashrujitpal@gmail.com>
2022-08-12 10:28:22 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAWYV5RVX6Y47W5OMN"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 }
